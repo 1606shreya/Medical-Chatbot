ed7e463c67d51feb32d328b62fde70df0ea7a092
unknown <bhagatshreya1606@gmail.com>
2025-09-18 12:18:27 +0530

    Folder=Structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical-Chatbot",
+    version="0.1",
+    author="SHREYA Bhagat",
+    packages=find_packages(),
+    install_requires=[]
+)
